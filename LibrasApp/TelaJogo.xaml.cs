cf509862dd07f1cd3b1bbd6028f77c4bb5ef5529
LucasBianchini <lucas_bianchini1@hotmail.com>
2016-11-30 15:00:25 -0200

    Background - imagem dentro*

--- LibrasApp/TelaJogo.xaml.cs
+++ LibrasApp/TelaJogo.xaml.cs
@@ -20,9 +20,9 @@ namespace LibrasApp
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class TelaJogo : Page
     {
-        public BlankPage1()
+        public TelaJogo()
         {
             this.InitializeComponent();
         }
